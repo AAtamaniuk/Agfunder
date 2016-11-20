54348d5bff777c448c69e528ff16f05d1d11efdc
AAtamaniuk <atol.if87@gmail.com>
2016-11-20 23:55:39 +0200

    FAQ page scroll and Company FAQ section ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
+$(document).ready(function(){
+  $(".JS-scroll-menu").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top - 80;
+    $('body,html').animate({scrollTop: top}, 1000);
+  });
+});
 
