889c192451cc7da1ff855a8de46142792d719541
AAtamaniuk <atol.if87@gmail.com>
2016-11-21 20:24:05 +0200

    FAQ page section Compani FAQ ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
   $(".JS-scroll-menu").on("click","a", function (event) {
     event.preventDefault();
+    $(".JS-sidebar-menu").removeClass("sidebar__menu--show-tablet");
     var id  = $(this).attr('href'),
     top = $(id).offset().top - 80;
     $('body,html').animate({scrollTop: top}, 1000);
   });
 });
 
+/*$(document).ready(function(){
+  $(".JS-sidebar-toggle").on("click", function (event) {
+    event.preventDefault();
+    $(".JS-sidebar-menu").toggle();
+  });
+});*/
+
+$(document).ready(function(){
+  $(".JS-sidebar-toggle").on("click", function (event) {
+    event.preventDefault();
+    $(".JS-sidebar-menu").toggleClass("sidebar__menu--show-tablet");
+  });
+});
+
+
+$(document).ready(function(){
+  $(".form-bg__close").on("click", function (event) {
+    event.preventDefault();
+    $(".form-bg").fadeOut();
+  });
+});
