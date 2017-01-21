1b6c033117650a945ea3829e6539f28ed4d2e996
AAtamaniuk <atol.if87@gmail.com>
2017-01-21 17:24:22 +0200

    html:build ready to work

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-// var server = require("browser-sync");
+var server = require("browser-sync");
 var mqpacker = require("css-mqpacker");
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
@@ -13,6 +13,8 @@ var svgmin = require("gulp-svgmin");
 var run = require("run-sequence");
 var del = require("del");
 var ghPages = require('gulp-gh-pages');
+var rigger = require('gulp-rigger');
+var reload = server.reload;
 
 var browsers = [
     "last 1 version",
@@ -23,6 +25,52 @@ var browsers = [
     "ie 11",
     "ie 10"
 ];
+
+var path = {
+  build: { //Тут мы укажем куда складывать готовые после сборки файлы
+    html: 'build/',
+    js: 'build/js/',
+    css: 'build/css/',
+    img: 'build/img/',
+    fonts: 'build/fonts/'
+  },
+  src: { //Пути откуда брать исходники
+    html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+    js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+    style: 'src/style/style.scss',
+    img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+    fonts: 'src/fonts/**/*.*'
+  },
+  watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+    html: 'src/**/*.html',
+    js: 'src/js/**/*.js',
+    style: 'src/style/**/*.scss',
+    img: 'src/img/**/*.*',
+    fonts: 'src/fonts/**/*.*'
+  },
+  clean: './build'
+};
+
+var config = {
+  server: {
+    baseDir: "./build"
+  },
+  tunnel: true,
+  host: 'localhost',
+  port: 9000,
+  logPrefix: "Frontend_Devil"
+};
+
+// Build for HTML
+gulp.task('html:build', function () {
+  gulp.src(path.src.html) //Выберем файлы по нужному пути
+    .pipe(rigger()) //Прогоним через rigger
+    .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+    .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
+});
+
+
+
 
 gulp.task("style", function() {
   gulp.src("src/sass/style.scss")
